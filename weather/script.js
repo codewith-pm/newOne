f0967777eeb3d8c697f169b4f025d02738378275
unknown <ponmuni.pm@outlook.com>
2024-10-27 07:49:52 -0700

    image update

--- weather/script.js
+++ weather/script.js
@@ -3,13 +3,14 @@ const city_input = document.querySelector('#city_input');
 const btn = document.querySelector('.btn');
 const cityname= document.querySelector('.city-name');
 const weather = document.querySelector('.weather');
-const sky_img = document.querySelector('.sky_img');
+const sky_img = document.querySelector('#sky_img');
 const weather_name = document.querySelector('.weather_name');
 const humidity = document.querySelector('.humidity');
 const wind = document.querySelector('.wind');
 
 const box = document.querySelector('.box');
 // box.style.display="none";
+window.onload= update();
 btn.addEventListener('click',update)
 
 async function update(){
@@ -23,9 +24,10 @@ async function update(){
         console.log(datass)
         let deg = datass.main.temp;
         let degVal=Math.floor(deg-273)+' °C'
-        
+        sky_img.src=`./asset/${datass.weather[0].main}.png`
         cityname.textContent=datass.name;
         weather.textContent=degVal;
+
         weather_name.textContent=datass.weather[0].main;
         humidity.textContent=datass.main.humidity;
         wind.textContent=datass.wind.speed+" km/h";
